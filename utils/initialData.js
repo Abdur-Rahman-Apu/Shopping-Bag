053e14dd1e3f95ebb66355c6078fa282e3eb6cce
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-09-17 19:49:22 +0600

    Product add and show into the UI completed

--- utils/initialData.js
+++ utils/initialData.js
@@ -1,7 +1,5 @@
-import { countElm } from "../lib/HtmlElements/elements.js";
-import { showShoppingItems } from "../lib/UI/shopItem.js";
+import { showShoppingItems, updateTotalProductNo } from "../lib/UI/shopItem.js";
 import { getFromLocalStorage, setIntoLocalStorage } from "./localStorage.js";
-import { setText } from "./UpdateUI.js";
 
 const initialData = () => {
   const products = getFromLocalStorage("products") || [];
@@ -10,7 +8,7 @@ const initialData = () => {
 
   showShoppingItems(products);
 
-  setText(countElm, products.length);
+  updateTotalProductNo();
 };
 
 export default initialData;
